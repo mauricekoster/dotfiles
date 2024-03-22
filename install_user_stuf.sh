ef1a9502fe4f72d0eabd56308666e444fa755fca
Maurice Koster <maurice@mauricekoster.com>
2024-03-22 19:22:52 +0100

    Stuf

--- install_user_stuf.sh
+++ install_user_stuf.sh
@@ -23,4 +23,13 @@ if [ ! -d "~/.pyenv" ]; then
 	popd
 fi
 
+# -- NVM - nodejs/npm
+# installs NVM (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# download and install Node.js
+nvm install 20
