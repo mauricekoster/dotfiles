af64f076dd1b8b1ec856a10369b9331c119ea43c
Maurice Koster <maurice@mauricekoster.com>
2024-09-06 11:54:41 +0200

    lazygit by brew, update neovim

--- bash/.bash/94_fzf_tmux.bashrc
+++ bash/.bash/94_fzf_tmux.bashrc
@@ -3,28 +3,29 @@
 #
 
 fzf_tmux() {
-  SELECTED_PROJECTS=$(tmuxinator list -n |
-      tail -n +2 |
-      fzf --prompt="Project: " -m -1 -q "$1")
+	SELECTED_PROJECTS=$(tmuxinator list -n |
+		tail -n +2 |
+		fzf --prompt="Project: " -m -1 -q "$1")
 
-  if [ -n "$SELECTED_PROJECTS" ]; then
-      # Set the IFS to \n to iterate over \n delimited projects
-      IFS=$'\n'
+	if [ -n "$SELECTED_PROJECTS" ]; then
+		# Set the IFS to \n to iterate over \n delimited projects
+		IFS=$'\n'
 
-      # Start each project without attaching
-      for PROJECT in $SELECTED_PROJECTS; do
-          tmuxinator start "$PROJECT" --no-attach # force disable attaching
-      done
+		# Start each project without attaching
+		for PROJECT in $SELECTED_PROJECTS; do
+			tmuxinator start "$PROJECT" --no-attach # force disable attaching
+		done
 
-      # If inside tmux then select session to switch, otherwise just attach
-      if [ -n "$TMUX" ]; then
-          SESSION=$(tmux list-sessions -F "#S" | fzf --prompt="Session: ")
-          if [ -n "$SESSION" ]; then
-              tmux switch-client -t "$SESSION"
-          fi
-      else
-          tmux attach-session
-      fi
-  fi
+		# If inside tmux then select session to switch, otherwise just attach
+		if [ -n "$TMUX" ]; then
+			SESSION=$(tmux list-sessions -F "#S" | fzf --prompt="Session: ")
+			if [ -n "$SESSION" ]; then
+				tmux switch-client -t "$SESSION"
+			fi
+		else
+			tmux attach-session
+		fi
+	fi
 }
 
+alias ts="fzf_tmux"
--- install_user_stuf.sh
+++ install_user_stuf.sh
@@ -1,26 +1,12 @@
-
-
 # --- Steam -------------------------------------------------------------------
 flatpak install -y flathub com.valvesoftware.Steam
 
-# --- Lazygit -----------------------------------------------------------------
-if [ ! -f "/usr/local/bin/lazygit" ]; then
-	info Installeer Lazygit
-	pushd ${DOWNLOAD_DIR}
-	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-	tar xf lazygit.tar.gz lazygit
-	sudo install lazygit /usr/local/bin
-	rm lazygit.tar.gz
-	popd
-fi
-
 # -- pyenv
 if [ ! -d "~/.pyenv" ]; then
-	info Pyenv installeren
-	pushd ~
-	curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-	popd
+  info Pyenv installeren
+  pushd ~
+  curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+  popd
 fi
 
 # -- NVM - nodejs/npm
@@ -28,8 +14,8 @@ fi
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
 # download and install Node.js
 nvm install 20
