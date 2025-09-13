8e2e6ebfde1a0fe424c75f020dc94d500c9fb756
Maurice Koster <maurice@mauricekoster.com>
2025-09-13 16:26:15 +0200

    then...

--- install/terminal/starship.sh
+++ install/terminal/starship.sh
@@ -2,7 +2,7 @@
 
 if ! has starship; then 
     brew install starship
-    if [ -f $HOME/.config/starship.toml ];
+    if [ -f $HOME/.config/starship.toml ]; then
         rm -f $HOME/.config/starship.toml
     fi
     mystow starship
