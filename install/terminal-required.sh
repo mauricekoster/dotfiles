3b4a53029d449b1d0c3f7871723e091c7f573789
Maurice Koster <maurice@mauricekoster.com>
2025-09-13 12:21:52 +0200

    term dep done 1

--- install/terminal-required.sh
+++ install/terminal-required.sh
@@ -10,7 +10,13 @@ fi
 if ! has brew; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+# Set Brew environment variables for install phase
+eval "$(/home/linuxbrew/.linuxbrew/bin brew shellenv)"
 
 if ! has gum; then
     brew install gum
+fi
+
+if ! has stow; then
+    brew install stow
 fi
--- install/terminal.sh
+++ install/terminal.sh
@@ -6,3 +6,4 @@ set -e
 source ${MKO_PATH}/install/terminal-required.sh
 
 echo "START INSTALLING TERMINAL APPS"
+for installer in ${MKO_PATH}/install/terminal/*.sh; do source $installer; done
