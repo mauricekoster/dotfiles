0d1fb4a5ecb341fc7fe5a859615548b97fa2f620
Maurice Koster <maurice@mauricekoster.com>
2025-09-19 17:12:21 +0200

    echoing

--- install/desktop/03-theme.sh
+++ install/desktop/03-theme.sh
@@ -2,11 +2,13 @@
 
 # Nordic
 if [ ! -d /usr/share/themes/Nordic ]; then
+    echo "Installing Nordic"
     sudo git clone https://github.com/EliverLara/Nordic.git /usr/share/themes/Nordic 2>&1 /dev/null
 fi
 
 # Icons
 if [ ! -d $HOME/.local/share/icons/Nordzy ]; then
+    echo "Installing Nordzy-icon"
     git clone https://github.com/alvatip/Nordzy-icon $HOME/Nordzy-icon 2>&1 /dev/null
     mypushd $HOME/Nordzy-icon
     ./install.sh 
@@ -16,6 +18,7 @@ fi
 
 # Cursors
 if [ ! -d $HOME/.icons/Nordzy-cursors ]; then
+    echo "Installing Nordzy-cursors"
     git clone https://github.com/alvatip/Nordzy-cursors $HOME/Nordzy-cursors 2>&1 /dev/null
     mypushd $HOME/Nordzy-cursors
     ./install.sh 
